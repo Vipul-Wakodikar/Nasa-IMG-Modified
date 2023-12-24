d49564515c073fa287ca05357aa08747831dbacd
Vipul-Wakodikar <wakodikar.vipul@gmail.com>
2023-12-24 23:51:37 +0530

    version-1.2-update

--- src/Components/MasonryLayout/PreviewModal.jsx
+++ src/Components/MasonryLayout/PreviewModal.jsx
@@ -2,21 +2,29 @@ import React, { useEffect, useState } from "react";
 import style from "./index.module.css";
 import ReactModal from "react-modal";
 
-const cardModal = ({ isOpen, data, onClose, appElement, key }) => {
+const customStyles = {
+  content: {
+    backgroundColor: '#26282f',
+    color: 'white',
+  },
+};
+
+const PreviewModal = ({ isOpen, data, onClose, appElement, key }) => {
   const [vidData, setVidData] = useState();
-  // console.log('datas', data.data[0])
   useEffect(() => {
     const fetchData = async () => {
-      const vidData = await fetch(data.href);
-      const json = await vidData.json();
-      const previewMP4 = json.find(
-        (element) =>
-          element.includes("orig.mp4") || element.includes("preview.mp4")
-      );
-      setVidData(previewMP4);
+      if (data && data.href) {
+        const vidData = await fetch(data.href);
+        const vidJson = await vidData.json();
+        const previewMP4 = vidJson.find(
+          (element) =>
+            element.includes("orig.mp4") || element.includes("preview.mp4")
+        );
+        setVidData(previewMP4);
+      }
     };
     fetchData();
-  }, [data]); // Update only when data changes
+  }, [data, data.href]); // Update only when data changes
   if (!isOpen) return null;
 
   const RenderVideoData = () => (
@@ -33,17 +41,17 @@ const cardModal = ({ isOpen, data, onClose, appElement, key }) => {
               Your browser does not support the video tag.
             </video>
           </div>
-          <div key={data.data.nasa_id}>
-            <h3>{data.data.title}</h3>
-            <p>Date Created: {data.data.date_created || "Not Found"}</p>
-            <p>Nasa Id: {data.data.nasa_id || "Not Found"}</p>
-            {data.data.keywords &&
-              data.data.keywords.map((keyword) => (
+          <div key={data.data[0].nasa_id}>
+            <h3>{data.data[0].title}</h3>
+            <p>Date Created: {data.data[0].date_created || "Not Found"}</p>
+            <p>Nasa Id: {data.data[0].nasa_id || "Not Found"}</p>
+            {data.data[0].keywords &&
+              data.data[0].keywords.map((keyword) => (
                 <div key={keyword}>{keyword}</div> // Use keyword as unique key for keyword divs
               ))}
             <p
               dangerouslySetInnerHTML={{
-                __html: `${data.data.description || "Not Found"}`,
+                __html: `${data.data[0].description || "Not Found"}`,
               }}
             />
           </div>
@@ -56,23 +64,23 @@ const cardModal = ({ isOpen, data, onClose, appElement, key }) => {
     <>
       {data && !vidData && (
         <>
-          <div key={data.data.nasa_id} className={style.modalContent}>
+          <div key={data.data[0].nasa_id} className={style.modalContent}>
             <img
               src={data.links[0].href || "#"}
-              alt={data.data.title}
+              alt={data.data[0].title}
               className={style.hdImgStyle}
             />
-            <div key={data.data.nasa_id}>
-              <h3>{data.data.title}</h3>
-              <p>Date Created: {data.data.date_created || "Not Found"}</p>
-              <p>Nasa Id: {data.data.nasa_id || "Not Found"}</p>
-              {data.data.keywords &&
-                data.data.keywords.map((keyword) => (
+            <div key={data.data[0].nasa_id}>
+              <h3>{data.data[0].title}</h3>
+              <p>Date Created: {data.data[0].date_created || "Not Found"}</p>
+              <p>Nasa Id: {data.data[0].nasa_id || "Not Found"}</p>
+              {data.data[0].keywords &&
+                data.data[0].keywords.map((keyword) => (
                   <div key={keyword}>{keyword}</div> // Use keyword as unique key for keyword divs
                 ))}
               <p
                 dangerouslySetInnerHTML={{
-                  __html: `${data.data.description || "Not Found"}`,
+                  __html: `${data.data[0].description || "Not Found"}`,
                 }}
               />
             </div>
@@ -83,7 +91,12 @@ const cardModal = ({ isOpen, data, onClose, appElement, key }) => {
   );
 
   return (
-    <ReactModal isOpen={isOpen} appElement={appElement} key={key}>
+    <ReactModal
+      isOpen={isOpen}
+      appElement={appElement}
+      key={key}
+      style={customStyles}
+    >
       <div className={style.closeButton}>
         <button onClick={onClose}>X</button>
       </div>
@@ -93,4 +106,4 @@ const cardModal = ({ isOpen, data, onClose, appElement, key }) => {
   );
 };
 
-export default cardModal;
+export default PreviewModal;
---
+++ src/Components/MasonryLayout/index.jsx
@@ -0,0 +1,88 @@
+import React, { useEffect, useState } from "react";
+import { addImageHeight } from "../Test2";
+import PhotoAlbum from "react-photo-album";
+import Modal from "./PreviewModal";
+import style from './index.module.css'
+
+const MasonLayout = ({url}) => {
+  const [recentData, setRecentData] = useState([]);
+  const [displayData, setDisplayData] = useState([]);
+  const [openModal, setOpenModal] = useState(false);
+  const [modalData, setModalData] = useState(null);
+
+  const openCardModal = (data) => {
+    setOpenModal(true);
+    if (data !== modalData) {
+      setModalData(data); // Update only if the data is different
+    }
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+    setModalData(null); // Reset modal data
+  };
+
+  const fetchData = async () => {
+    const recent = await fetch(url);
+    const recentJson = await recent.json();
+    setRecentData(recentJson);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  function filterNonImageData() {
+    // get the items array from the recentData object, or use an empty array if it is undefined
+    const itemsArray = recentData?.collection?.items || [];
+    // filter the items array by removing the elements that have the same href value as their links[0].href value
+    const hrefArray = itemsArray.map((item) => ({
+      ...item,
+      src: item.links[0].href,
+    }));
+    return hrefArray;
+  }
+
+  useEffect(() => {
+    addImageHeight(filterNonImageData())
+      .then((newArray) => {
+        setDisplayData(newArray);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, [recentData]);
+  return (
+    <>
+      <PhotoAlbum
+        layout="masonry"
+        photos={displayData}
+        renderPhoto={({ photo, wrapperStyle, renderDefaultPhoto }) => {
+          // console.log('first', photo)
+          return (
+            <>
+              <button
+                className={style.cardButtonStyle}
+                onClick={() => {
+                  if (JSON.stringify(modalData) !== JSON.stringify(photo)) {
+                    openCardModal(photo || null);
+                  }
+                }}
+              >
+                {renderDefaultPhoto({ wrapped: true })}
+              </button>
+            </>
+          );
+        }}
+      />
+      {openModal && modalData && (<Modal
+        isOpen={openModal}
+        data={modalData}
+        onClose={handleCloseModal}
+        appElement={document.getElementById("root")}
+      />)}
+    </>
+  );
+};
+
+export default MasonLayout;
